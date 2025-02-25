253ead1da845216f1926c77fc6334c4db8d58465
gastonmiguel <gastonmiguel@live.com.ar>
2025-02-24 21:31:56 -0300

    delete project

--- resources/js/Components/ApplicationLogo.tsx
+++
@@ -1,7 +0,0 @@
-export default function ApplicationLogo() {
-    return (
-        <span className="text-3xl font-bold text-fuchsia-600 dark:text-fuchsia-400">
-            SebKy
-        </span>
-    );
-}
